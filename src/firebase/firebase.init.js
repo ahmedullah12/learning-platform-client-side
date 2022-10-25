0cfc0fd297240bd8a173d6cc3be0e9d27a9f8d69
ahmedullah12 <gulamjoy321@gmail.com>
2022-10-25 18:47:51 +0600

    router, firebase, bootstrap and react icons installed

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgY8pX_M-7KaZZC1MlRND8i6mour9N9LQ",
+  authDomain: "assignment-10-client-sid-ef424.firebaseapp.com",
+  projectId: "assignment-10-client-sid-ef424",
+  storageBucket: "assignment-10-client-sid-ef424.appspot.com",
+  messagingSenderId: "869087840186",
+  appId: "1:869087840186:web:575bb554ac658bcf2a7f83"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
